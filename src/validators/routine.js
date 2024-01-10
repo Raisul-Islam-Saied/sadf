3dbd93fab717b878ce77a1fcbc2d0bc2a3a0fb7d
raisul-islam-saied <raisulislamsaied>
2024-01-10 15:23:23 +0600

    dflsh

--- src/validators/routine.js
+++ src/validators/routine.js
@@ -1,40 +1,40 @@
 const createError = require("http-errors");
-const { check } = require('express-validator')
+const { body } = require('express-validator')
 const { nameOfClass, day, priority, sakha } = require('../models/data.json')
 const User = require('../models/userModel')
 const bcrypt = require('bcryptjs')
 const addRotineValidation = [
-    check("subject")
+    body("subject")
         .isLength({ min: 1 })
         .withMessage("Name is required")
         .isLength({ min: 3, max: 30 })
         .withMessage("name must should 3 to 30 character")
         .trim(),
 
-    check("teacher")
+    body("teacher")
         .isLength({ min: 1 })
         .withMessage("teacher name is required")
         .isLength({ min: 3, max: 30 })
         .withMessage("name must should 3 to 30 character")
         .trim(),
-    check("teacher_id").isLength({ min: 8, max: 8 }).withMessage('id must be 8 character'),
+    body("teacher_id").isLength({ min: 8, max: 8 }).withMessage('id must be 8 character'),
 
-    check("day")
+    body("day")
         .notEmpty()
         .withMessage("day name is required").isIn(day)
     ,
 
-    check("from")
+    body("from")
         .isTime().withMessage('not a valid time')
     ,
-    check("to")
+    body("to")
         .isTime().withMessage('not a valid time')
     ,
-    check('priority')
-        .notEmpty().isNumeric().withMessage('not a valid priority'),
-    check('sakha')
+    body('priority')
+        .notEmpty().isIn([1, 2, 3, 4, 5, 6, 7]).withMessage('not a valid priority'),
+    body('sakha')
         .notEmpty().isIn(sakha).withMessage('not a valid sakha'),
-    check("nameOfClass")
+    body("nameOfClass")
         .notEmpty()
         .withMessage("name of class is required")
         .isAlpha("bn-BD", { ignore: " -," })
